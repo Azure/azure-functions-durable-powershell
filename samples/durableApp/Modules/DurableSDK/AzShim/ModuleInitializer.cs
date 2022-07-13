e358ed1f645bc502c95ef304e16e8233c506b302
Michael Peng <mpeng029@gmail.com>
2022-07-13 16:27:58 -0700

    Clean up unused members and add comments to assembly-loading code

--- samples/durableApp/Modules/DurableSDK/AzShim/ModuleInitializer.cs
+++ samples/durableApp/Modules/DurableSDK/AzShim/ModuleInitializer.cs
@@ -1,49 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Management.Automation;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DurableSDK
 {
-    public class ModuleInitializer : IModuleAssemblyInitializer
+    // This project component separates the dependencies of the Durable SDK from those of the PowerShell worker.
+    // See https://docs.microsoft.com/en-us/powershell/scripting/dev-cross-plat/resolving-dependency-conflicts?view=powershell-7.2
+    // for more detail.
+    public class ModuleInitializer : IModuleAssemblyInitializer, IModuleAssemblyCleanup
     {
         private static string s_binBasePath = Path.GetFullPath(
             Path.Combine(
                 Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-            "dependencies"));
-
-        //private static string s_binCommonPath = Path.Combine(s_binBasePath, "Common");
-
-        //private static string s_binCorePath = Path.Join(s_binBasePath, "Core");
+            "Dependencies"));
 
         public void OnImport()
         {
-            AssemblyLoadContext.Default.Resolving += ResolveAssembly_NetCore;
+            // Add the Resolving event handler here
+            AssemblyLoadContext.Default.Resolving += ResolveDurableEngineAssembly;
         }
 
         public void OnRemove(PSModuleInfo psModuleInfo)
         {
             // Remove the Resolving event handler here
-            AssemblyLoadContext.Default.Resolving -= ResolveAssembly_NetCore;
+            AssemblyLoadContext.Default.Resolving -= ResolveDurableEngineAssembly;
         }
 
-        private static Assembly ResolveAssembly_NetCore(
+        private static Assembly ResolveDurableEngineAssembly(
             AssemblyLoadContext assemblyLoadContext,
             AssemblyName assemblyName)
         {
             // In .NET Core, PowerShell deals with assembly probing so our logic is much simpler
-            // We only care about our Engine assembly
+            // We only want to resolve the DurableEngine.dll assembly, which will be loaded into
+            // the custom ALC.
             if (!assemblyName.Name.Equals("DurableEngine"))
             {
                 return null;
             }
 
-            // Now load the Engine assembly through the dependency ALC, and let it resolve further dependencies automatically
+            // We load the Durable Engine assembly through the Dependency ALC, the context in which
+            // all of its dependencies will be resolved (preventing potential conflicts with the
+            // PowerShell worker's dependencies). The DurableEngine.dll will then be passed into
+            // PowerShell's ALC.
             return DependencyAssemblyLoadContext.GetForDirectory(s_binBasePath).LoadFromAssemblyName(assemblyName);
         }
     }
