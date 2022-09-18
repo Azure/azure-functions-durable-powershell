8d8496f1cd34a4da2e993fdb873802609e5cd406
David Justo <david.justo.1996@gmail.com>
2022-09-18 15:43:51 -0700

    simplify impl

--- samples/durableApp/Modules/DurableSDK/DurableEngine/Commands/WaitDurableTaskCommand.cs
+++ samples/durableApp/Modules/DurableSDK/DurableEngine/Commands/WaitDurableTaskCommand.cs
@@ -3,40 +3,40 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#pragma warning disable 1591 // Missing XML comment for publicly visible type or member 'member'
+//#pragma warning disable 1591 // Missing XML comment for publicly visible type or member 'member'
 
-namespace DurableEngine.Commands
-{
-    using DurableEngine;
-    using System.Collections;
-    using System.Management.Automation;
+//namespace DurableEngine.Commands
+//{
+//    using DurableEngine;
+//    using System.Collections;
+//    using System.Management.Automation;
 
-    public class WaitDurableTaskCommand2 : DFCommand
-    {
-        public WaitDurableTaskCommand2(DurableSDKTask[] task, SwitchParameter any, SwitchParameter noWait, Hashtable privateData) : base(noWait, privateData)
-        {
-            Task = task;
-            Any = any;
-        }
+//    public class WaitDurableTaskCommand2 : DFCommand
+//    {
+//        public WaitDurableTaskCommand2(DurableSDKTask[] task, SwitchParameter any, SwitchParameter noWait, Hashtable privateData) : base(noWait, privateData)
+//        {
+//            Task = task;
+//            Any = any;
+//        }
 
-        public DurableSDKTask[] Task { get; set; }
+//        public DurableSDKTask[] Task { get; set; }
 
-        public SwitchParameter Any { get; set; }
+//        public SwitchParameter Any { get; set; }
 
-        internal override DurableSDKTask GetTask()
-        {
-            var sdkContext = getOrchestrationContext();
-            DurableSDKTask task = null;
-            if (Any)
-            {
-                task = new WhenAnyTask(Task, sdkContext);
-            }
-            else
-            {
-                task = new WhenAllTask(Task, sdkContext);
-            }
-            return task;
-        }
+//        internal override DurableSDKTask GetTask()
+//        {
+//            var sdkContext = getOrchestrationContext();
+//            DurableSDKTask task = null;
+//            if (Any)
+//            {
+//                task = new WhenAnyTask(Task, sdkContext);
+//            }
+//            else
+//            {
+//                task = new WhenAllTask(Task, sdkContext);
+//            }
+//            return task;
+//        }
 
-    }
-}
+//    }
+//}
