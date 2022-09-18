7dae8a03e48ed70ae8cfdb9adff6c61cf2e5d625
David Justo <david.justo.1996@gmail.com>
2022-09-18 15:45:58 -0700

    comment out wip

--- samples/durableApp/Modules/DurableSDK/DurableEngine/Tasks/WhenAllTask.cs
+++ samples/durableApp/Modules/DurableSDK/DurableEngine/Tasks/WhenAllTask.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.DurableTask;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.DurableTask;
 
-namespace DurableEngine
-{
-    internal class WhenAllTask : DurableSDKTask
-    {
-        internal DurableSDKTask[] tasks;
+//namespace DurableEngine
+//{
+//    internal class WhenAllTask : DurableSDKTask
+//    {
+//        internal DurableSDKTask[] tasks;
 
-        internal override object Result {
-            get
-            {
-                List<object> allResults = new List<object>();
-                foreach (var subTask in this.tasks)
-                {
-                    var res = subTask.Result;
-                    allResults.Add(res);
-                }
-                return allResults; // cache
-            }
-        }
+//        internal override object Result {
+//            get
+//            {
+//                List<object> allResults = new List<object>();
+//                foreach (var subTask in this.tasks)
+//                {
+//                    var res = subTask.Result;
+//                    allResults.Add(res);
+//                }
+//                return allResults; // cache
+//            }
+//        }
 
-        internal override Exception Exception => throw new NotImplementedException();
+//        internal override Exception Exception => throw new NotImplementedException();
 
-        internal WhenAllTask(DurableSDKTask[] tasks, OrchestrationContext sdkContext) :
-            base(sdkContext)
-        {
-            this.tasks = tasks;
-        }
+//        internal WhenAllTask(DurableSDKTask[] tasks, OrchestrationContext sdkContext) :
+//            base(sdkContext)
+//        {
+//            this.tasks = tasks;
+//        }
 
-        internal override OrchestrationAction CreateOrchestrationAction()
-        {
-            var compoundActions = tasks.Select((task) => {
+//        internal override OrchestrationAction CreateOrchestrationAction()
+//        {
+//            var compoundActions = tasks.Select((task) => {
 
-                return task.CreateOrchestrationAction();
-            }).ToArray();
-            var action = new WhenAllAction(compoundActions);
-            return action;
-        }
+//                return task.CreateOrchestrationAction();
+//            }).ToArray();
+//            var action = new WhenAllAction(compoundActions);
+//            return action;
+//        }
 
-        internal override Task createDTFxTask()
-        {
-            var innertasks = this.tasks.Select(task => task.getDTFxTask());
-            var task = Task.WhenAll(innertasks);
-            return task;
-        }
-    }
-}
+//        internal override Task createDTFxTask()
+//        {
+//            var innertasks = this.tasks.Select(task => task.getDTFxTask());
+//            var task = Task.WhenAll(innertasks);
+//            return task;
+//        }
+//    }
+//}
