3df3f255c45d1b7b9de299d26e6d897091fb5510
Michael Peng <mpeng029@gmail.com>
2022-11-23 11:08:39 -0800

    Move durableApp to the test folder

---
+++ test/E2E/durableApp/DurableAppHttpStart/run.ps1
@@ -0,0 +1,19 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+$ErrorActionPreference = 'Stop'
+
+$OrchestratorName = $Request.Params.OrchestratorName
+$InstanceId = Start-DurableOrchestrationExternal -FunctionName $OrchestratorName -InputObject 'Hello'
+Write-Host "Started orchestration with ID = '$InstanceId'"
+
+$Response = New-DurableOrchestrationCheckStatusResponseExternal -Request $Request -InstanceId $InstanceId
+Push-OutputBinding -Name Response -Value $Response
+
+$Status = Get-DurableStatusExternal -InstanceId $InstanceId
+Write-Host "Orchestration $InstanceId status: $($Status | ConvertTo-Json)"
+if ($Status.RuntimeStatus -notin 'Pending', 'Running', 'Failed', 'Completed') {
+    throw "Unexpected orchestration $InstanceId runtime status: $($Status.RuntimeStatus)"
+}
+
+Write-Host "DurableAppHttpStartCompleted"
