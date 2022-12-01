31714b89ec9e35175d5ec393ba147f0451056269
Michael Peng <mpeng029@gmail.com>
2022-12-01 13:38:35 -0800

    Add back DurableTimer and Wait-DurableTask test cases

--- samples/durableApp/DurableFunctionsOrchestrator/run.ps1
+++ samples/durableApp/DurableFunctionsOrchestrator/run.ps1
@@ -8,39 +8,39 @@ Write-Host "Beginning first activity function"
 $output += Invoke-DurableActivityE -FunctionName 'Hello' -Input 'Seattle'
 Write-Host "Passed first activity function"
 
-# # TEST CASE 1: Basic Start-DurableTimer
-# Write-Host "TEST 1: Basic Start-DurableTimer"
-# Write-Host "Started durable timer at $($Context.CurrentUtcDateTime)"
-# Start-DurableTimerE -Duration (New-Timespan -Seconds 5)
-# Write-Host "Finished awaiting timer at $($Context.CurrentUtcDateTime)"
+# TEST CASE 1: Basic Start-DurableTimer
+Write-Host "TEST 1: Basic Start-DurableTimer"
+Write-Host "Started durable timer at $($Context.CurrentUtcDateTime)"
+Start-DurableTimerE -Duration (New-Timespan -Seconds 5)
+Write-Host "Finished awaiting timer at $($Context.CurrentUtcDateTime)"
 
-# # TEST CASE 2: WaitAll for two activity functions and two timers
-# Write-Host "TEST 2: WaitAll for two activity functions and two timers"
-# $activity1 = Invoke-DurableActivityE -FunctionName "Hello" -Input "Test 2 Activity 1" -NoWait
-# $activity2 = Invoke-DurableActivityE -FunctionName "Hello" -Input "Test 2 Activity 2" -NoWait
-# $timer1 = Start-DurableTimerE -Duration (New-Timespan -Seconds 10) -NoWait
-# Write-Host "Started durable timer 1 at $($Context.CurrentUtcDateTime)"
-# $timer2 = Start-DurableTimerE -Duration (New-Timespan -Seconds 5) -NoWait
-# Write-Host "Started durable timer 2 $($Context.CurrentUtcDateTime)"
-# $allTasks = @($activity1, $activity2, $timer1, $timer2)
-# $allTaskResults = Wait-DurableTaskE -Task $allTasks
-# if ($allTaskResults.Count -ne $allTasks.Count)
-# {
-#     throw "The WaitAll output array length is $($allTaskResults.Count) rather than $($allTasks.Count), as expected."
-# }
-# $output += $allTaskResults
-# Write-Host "Finished awaiting timers at $($Context.CurrentUtcDateTime)"
+# TEST CASE 2: WaitAll for two activity functions and two timers
+Write-Host "TEST 2: WaitAll for two activity functions and two timers"
+$activity1 = Invoke-DurableActivityE -FunctionName "Hello" -Input "Test 2 Activity 1" -NoWait
+$activity2 = Invoke-DurableActivityE -FunctionName "Hello" -Input "Test 2 Activity 2" -NoWait
+$timer1 = Start-DurableTimerE -Duration (New-Timespan -Seconds 10) -NoWait
+Write-Host "Started durable timer 1 at $($Context.CurrentUtcDateTime)"
+$timer2 = Start-DurableTimerE -Duration (New-Timespan -Seconds 5) -NoWait
+Write-Host "Started durable timer 2 $($Context.CurrentUtcDateTime)"
+$allTasks = @($activity1, $activity2, $timer1, $timer2)
+$allTaskResults = Wait-DurableTaskE -Task $allTasks
+if ($allTaskResults.Count -ne $allTasks.Count)
+{
+    throw "The WaitAll output array length is $($allTaskResults.Count) rather than $($allTasks.Count), as expected."
+}
+$output += $allTaskResults
+Write-Host "Finished awaiting timers at $($Context.CurrentUtcDateTime)"
 
-# # TEST CASE 3: WaitAny for two timers
-# Write-Host "TEST 3: WaitAny for two timers"
-# $timer1 = Start-DurableTimerE -Duration (New-Timespan -Seconds 10) -NoWait
-# Write-Host "Started durable timer 1 at $($Context.CurrentUtcDateTime)"
-# $timer2 = Start-DurableTimerE -Duration (New-Timespan -Seconds 5) -NoWait
-# Write-Host "Started durable timer 2 $($Context.CurrentUtcDateTime)"
-# $winner = Wait-DurableTaskE -Task @($timer1, $timer2) -Any
-# Write-Host "$($winner -eq $timer2)"
-# Write-Host "Finished awaiting timers at $($Context.CurrentUtcDateTime)"
-# Stop-DurableTimerTaskE -Task $timer1
-# Write-Host 'Stopped durable timer'
+# TEST CASE 3: WaitAny for two timers
+Write-Host "TEST 3: WaitAny for two timers"
+$timer1 = Start-DurableTimerE -Duration (New-Timespan -Seconds 10) -NoWait
+Write-Host "Started durable timer 1 at $($Context.CurrentUtcDateTime)"
+$timer2 = Start-DurableTimerE -Duration (New-Timespan -Seconds 5) -NoWait
+Write-Host "Started durable timer 2 $($Context.CurrentUtcDateTime)"
+$winner = Wait-DurableTaskE -Task @($timer1, $timer2) -Any
+Write-Host "$($winner -eq $timer2)"
+Write-Host "Finished awaiting timers at $($Context.CurrentUtcDateTime)"
+Stop-DurableTimerTaskE -Task $timer1
+Write-Host 'Stopped durable timer'
 
 $output
