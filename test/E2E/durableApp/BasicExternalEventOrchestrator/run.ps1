c9913d0493575fe32e2e31e797ed494575976ba4
David Justo <david.justo.1996@gmail.com>
2023-04-17 11:19:49 -0700

    Patch externalevent and InstanceId e2e test (#40)

--- test/E2E/durableApp/BasicExternalEventOrchestrator/run.ps1
+++ test/E2E/durableApp/BasicExternalEventOrchestrator/run.ps1
@@ -2,9 +2,6 @@ param($Context)
 
 $output = @()
 
-Invoke-DurableActivityE -FunctionName "Hello" -Input "Tokyo"
-Invoke-DurableActivityE -FunctionName "Hello" -Input "Seattle"
 $output += Start-DurableExternalEventListenerE -EventName "TESTEVENTNAME" 
-Invoke-DurableActivityE -FunctionName "Hello" -Input "London"
 
 $output
