8408a3b3b70dfe35ec414412a4aa09c972376a97
Michael Peng <michaelpeng@microsoft.com>
2023-05-08 09:24:46 -0700

    Make external event tests more reliable by decreasing the time to send event

--- test/E2E/durableApp/DurableClientSendExternalEvent/run.ps1
+++ test/E2E/durableApp/DurableClientSendExternalEvent/run.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "DurableClientSendExternalEvent started"
 
-$OrchestratorInputs = @{ FirstDuration = 5; SecondDuration = 60 }
+$OrchestratorInputs = @{ FirstDuration = 5; SecondDuration = 30 }
 
 $InstanceId = Start-DurableOrchestration -FunctionName "SendDurableExternalEventOrchestrator" -InputObject $OrchestratorInputs
 Write-Host "Started orchestration with ID = '$InstanceId'"
@@ -11,7 +11,7 @@ Write-Host "Started orchestration with ID = '$InstanceId'"
 $Response = New-DurableOrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
 Push-OutputBinding -Name Response -Value $Response
 
-Start-Sleep -Seconds 15
+Start-Sleep -Seconds 10
 Send-DurableExternalEvent -InstanceId $InstanceId -EventName "SecondExternalEvent"
 
 Write-Host "DurableClientSendExternalEvent completed"
